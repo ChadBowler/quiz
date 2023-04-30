da5ef170a4ef781d7a67e7db7818440f0c4d3617
Chad Bowler <chadbowler@gmail.com>
2023-04-30 15:40:46 -0600

    Added basic pages with frame built, added json file

---
+++ assets/script/script.js
@@ -0,0 +1,17 @@
+const data = JSON.parse('/quiz-questions.json');
+import * as dataModule from './quiz-questions.json' assert{type: 'json'};
+
+// "number": 1,
+//       "question": "What is the HTML tag for the most important heading?",
+//       "answers": [
+//         "`<h1>`",
+//         "`<h2>`",
+//         "`<h3>`",
+//         "`<h4>`"
+//       ],
+//       "correct_answer": 1
+
+
+
+console.log(data);
+console.log(dataModule);
