b8e233a3bb5faa29ecdb3d65ecf3a43a97a8d809
Chad Bowler <chadbowler@gmail.com>
2023-05-01 14:54:20 -0600

    Moved json data to script, added functions to loop through questions.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,392 @@
-const data = JSON.parse('/quiz-questions.json');
-import * as dataModule from './quiz-questions.json' assert{type: 'json'};
+var score = 0;
 
-// "number": 1,
-//       "question": "What is the HTML tag for the most important heading?",
-//       "answers": [
-//         "`<h1>`",
-//         "`<h2>`",
-//         "`<h3>`",
-//         "`<h4>`"
-//       ],
-//       "correct_answer": 1
+const questions =
+    [ 
+      {
+        number: 1,
+        question: "What is the HTML tag for the most important heading?",
+        answers: [
+          "h1",
+          "h2",
+          "h3",
+          "h4"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 2,
+        question: "What is the CSS property for setting the font size of an element?",
+        answers: [
+          "`font-size`",
+          "`font-weight`",
+          "`font-family`",
+          "`color`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 3,
+        question: "What is the JavaScript function for creating a new element?",
+        answers: [
+          "`document.createElement()`",
+          "`document.getElementById()`",
+          "`document.appendChild()`",
+          "`document.removeChild()`"
+        ],
+        correct_answer: 1
+      },      {
+        number: 4,
+        question: "What is the HTML tag for a paragraph?",
+        answers: [
+          "p",
+          "div",
+          "span",
+          "ul"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 5,
+        question: "What is the CSS property for setting the background color of an element?",
+        answers: [
+          "`background-color`",
+          "`background-image`",
+          "`background-repeat`",
+          "`background-position`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 6,
+        question: "What is the JavaScript function for getting the value of an input element?",
+        answers: [
+          "`document.getElementById('input').value`",
+          "`document.getElementById('input').innerHTML`",
+          "`document.getElementById('input').className`",
+          "`document.getElementById('input').style`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 7,
+        question: "What is the HTML tag for a link?",
+        answers: [
+          "a",
+          "b",
+          "i",
+          "u"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 8,
+        question: "What is the CSS property for setting the text alignment of an element?",
+        answers: [
+          "`text-align`",
+          "`text-decoration`",
+          "`text-transform`",
+          "`text-shadow`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 9,
+        question: "What is the JavaScript function for adding an event listener to an element?",
+        answers: [
+          "`document.getElementById('element').addEventListener('click', function() { });`",
+          "`document.getElementById('element').onclick = function() { };`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 10,
+        question: "What is the HTML tag for a table?",
+        answers: [
+          "table",
+          "tr",
+          "td",
+          "th"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 11,
+        question: "What is the CSS property for setting the width of an element?",
+        answers: [
+          "`width`",
+          "`height`",
+          "`margin`",
+          "`padding`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 12,
+        question: "What is the JavaScript function for creating a new event?",
+        answers: [
+          "`new Event('click')`",
+          "`new Event('mousedown')`",
+          "`new Event('mousemove')`",
+          "`new Event('mouseup')"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 13,
+        question: "What is the HTML tag for a list?",
+        answers: [
+          "ul",
+          "ol",
+          "li",
+          "dl"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 14,
+        question: "What is the CSS property for setting the list style type of a list?",
+        answers: [
+          "`list-style-type`",
+          "`list-style-position`",
+          "`list-style-image`",
+          "`list-style-image`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 15,
+        question: "What is the JavaScript function for getting the number of elements in an array?",
+        answers: [
+          "`array.length`",
+          "`array.push()`",
+          "`array.pop()`",
+          "`array.sort()`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 16,
+        question: "What is the HTML tag for an image?",
+        answers: [
+          "img",
+          "video",
+          "audio",
+          "canvas"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 17,
+        question: "What is the CSS property for setting the height of an image?",
+        answers: [
+          "`height`",
+          "`width`",
+          "`margin`",
+          "`padding`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 18,
+        question: "What is the JavaScript function for creating a new image?",
+        answers: [
+          "`new Image()`",
+          "`new Canvas()`",
+          "`new Audio()`",
+          "`new Video()`"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 19,
+        question: "What is the HTML tag for a form?",
+        answers: [
+          "form",
+          "input",
+          "textarea",
+          "select"
+        ],
+        correct_answer: 1
+      },
+      {
+        number: 20,
+        question: "What is the CSS property for setting the border of an element?",
+        answers: [
+          "border",
+          "border-width",
+          "border-color",
+          "border-style"
+        ],
+        correct_answer: 1
+      }
+    
+    ]
+    
+var current = 0;
+var answerButton1 ="";
+var answerButton2 ="";
+var answerButton3 ="";
+var answerButton4 ="";
+function startGame(){
+    
+    var quizbutton = document.getElementById("quizbutton");
+    var readyHeader = document.getElementById("readyheader"); 
+    quizbutton.style.display = "none";
+    readyHeader.style.display = "none";
+    
+    quizQuestion(current);
+}
+
+
+function quizQuestion(current){
+  
+  
+  function getAnswer1(){
+    answerButton1.removeEventListener("click", getAnswer1);
+    var count = current;
+    var answer = 1;
+    console.log(current);
+    checkAnswer(count, answer);
+  }
+  function getAnswer2(){
+    answerButton2.removeEventListener("click", getAnswer2);
+    var count = current;
+    var answer = 2;
+    console.log(current);
+    checkAnswer(count, answer);
+  }
+  function getAnswer3(){
+    answerButton3.removeEventListener("click", getAnswer3);
+    var count = current;
+    var answer = 3;
+    console.log(current);
+    checkAnswer(count, answer);
+  }
+  function getAnswer4(){
+    answerButton4.removeEventListener("click", getAnswer4);
+    var count = current;
+    var answer = 4;
+    console.log(current);
+    checkAnswer(count, answer);
+  }
+  
+
+  answerButton1 = document.getElementById("answerbutton1")
+  answerButton2 = document.getElementById("answerbutton2")
+  answerButton3 = document.getElementById("answerbutton3")
+  answerButton4 = document.getElementById("answerbutton4")
+
+     
+    
+    
+    
+
+  console.log(answerButton1);
+
+    console.log("CurrentA = "+current);
+    document.getElementById("number").innerHTML = questions[current].number;
+    document.getElementById("question").innerHTML = questions[current].question;
+    document.getElementById("answer1").innerHTML = questions[current].answers[0];
+    document.getElementById("answer2").innerHTML = questions[current].answers[1];
+    document.getElementById("answer3").innerHTML = questions[current].answers[2];
+    document.getElementById("answer4").innerHTML = questions[current].answers[3];
+      
+    answerButton1.addEventListener("click", getAnswer1); 
+    answerButton2.addEventListener("click", getAnswer2);
+    answerButton3.addEventListener("click", getAnswer3);
+    answerButton4.addEventListener("click", getAnswer4);
+  
+    
+}
 
 
 
-console.log(data);
-console.log(dataModule);
+function checkAnswer(current, answer){
+  
+    console.log("CurrentC = "+current);
+    console.log("Current Question number "+(current+1));
+    console.log("You answered: " + answer);
+    console.log("Correct answer: "+questions[current].correct_answer);
+
+    if(answer==questions[current].correct_answer){
+       score+=1;
+        console.log("correct"); 
+        console.log("Your score: "+score);  
+    } else{
+        console.log("incorrect");   
+    }
+    if(current<questions.length){
+        current+=1;
+        console.log("CurrentD = "+current);
+        console.log("test");
+        quizQuestion(current);
+    }
+    else{
+        console.log("Game Over");               
+    }
+    // return current;
+    
+}
+
+// async function checkAnswer(questions){
+//     let clickedButton;
+//     clickedButton= document.getElementById("answerbutton1").addEventListener("click", function(){
+//         clickedButton = 1;
+//         console.log("correct");
+//     });
+//     document.getElementById("answerbutton2").addEventListener("click", function(){
+//         clickedButton = 2;
+//     });
+//     document.getElementById("answerbutton3").addEventListener("click", function(){
+//         clickedButton = 3;
+//     });
+//     document.getElementById("answerbutton4").addEventListener("click", function(){
+//         clickedButton = 4;
+//     });
+
+//     await clickedButton;
+//     if(clickedButton == questions.correct_answer){
+//         score +=1;
+//     }
+//     console.log(clickedButton);
+//     return score;
+// }
+// console.log(score);
+
+
+
+
+
+
+
+
+
+//   console.log(questions[0]);
+
+//   for(var i=0;i<questions.length;i++){
+//     // console.log(questions[i].correct_answer);
+//     for(var x=0;x<questions[i].answers.length;x++){
+//         if((x+1)==questions[i].correct_answer){ 
+//             console.log(questions[i].question);
+//             console.log(questions[i].answers[x]);
+//         }
+        // else{
+        //     console.log('huh?')
+        // }
+//     }
+//   }
+
+//   const obj = [{
+//         hero: "Superman",
+//         powers:["Strength", "Speed", "Heat Vision", "Cold Breath"],
+//         powerlvl: 85
+//     },
+//     {
+//         hero: "Wonder Woman",
+//         powers:["Strength", "Speed", "Lasso of Truth", "Invisible Jet"],
+//         powerlvl: 95
+//     }
+//   ]
+//     console.log(obj[1]);
+
+  
